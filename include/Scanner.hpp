0365d8524ccc5839898a6a3e889b7732fe6013a9
OLDSPICECODE <56983980+OLDSPICECODE@users.noreply.github.com>
2023-05-11 19:18:04 -0500

    5/11

--- include/Scanner.hpp
+++ include/Scanner.hpp
@@ -6,24 +6,33 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <stack>
 
 class Scanner
 {
 private:
     std::fstream input_file;
     std::string current_line;
+    std::stack<int> indents;
+
     size_t line_pos;
     size_t line_count;
     size_t token_row;
     size_t token_col;
+    size_t tab_size;
+    int    indent_level;
 
     // flags
-    bool debug;     // indica modo debug
-    bool eof;       // indica fin de input_file
-    bool is_logic;  // indica si la linea actual es logica
+    bool debug;      // indica modo debug
+    bool eof;        // indica fin de input_file
+    bool is_logic;   // indica si la linea actual es logica
+    bool validating; // indica que hay una posible indentacion
+    bool is_first;
 
     void throwError(std::string msg);
     void nextLine();
+    void getIndentLevel();
+    bool validateLogic();
 
     int currChar();
     int moveChar();
